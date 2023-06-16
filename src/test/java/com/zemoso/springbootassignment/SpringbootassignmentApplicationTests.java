21b3718ca8fa53190630a1d7acc27fb9545b6e14
Ayush-Bajpai47 <ayush.bajpai@zemosolabs.com>
2023-06-16 11:19:07 +0530

    springboot files changed

---
+++ src/test/java/com/zemoso/springbootassignment/SpringbootassignmentApplicationTests.java
@@ -0,0 +1,21 @@
+package com.zemoso.springbootassignment;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+class SpringbootassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+
+}
